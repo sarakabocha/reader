b1678eeab7ddec3bba950d67718b15d3674288b5
sara <7968635+sarakabocha@users.noreply.github.com>
2025-03-17 08:59:37 -0700

    set base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/reader/",
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-  }
+  },
 });
