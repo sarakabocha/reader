4952faf0a48b26e47dd1840c6ac9ca8d50e2e6fd
sara <7968635+sarakabocha@users.noreply.github.com>
2025-03-23 00:03:27 -0700

    more writing samples

--- src/data/poetry/index.ts
+++ src/data/poetry/index.ts
@@ -1,10 +1,13 @@
 import onion from "./onion";
 import snow from "./snow";
+import spring from "./spring";
 import catinanemptyapt from "./catinanemptyapt";
+import prologue from "./prologue";
+import thanksgivingprayer from "./thanksgivingprayer";
 
 import { Collection } from "../collection";
 
 export default {
   name: "Poetry",
-  works: [onion, snow, catinanemptyapt],
+  works: [prologue, snow, spring, onion, catinanemptyapt, thanksgivingprayer],
 } as Collection;
---
+++ src/data/poetry/spring.ts
@@ -0,0 +1,45 @@
+import { Work } from "../collection";
+
+const original = `봄이 혈관 속에 시내처럼 흘러
+돌 , 돌, 시내 가차운 언덕에
+개나리, 진달래, 노오란 배추꽃
+
+삼동(三冬)을 참어온 나는
+풀포기처럼 피어난다.
+
+즐거운 종달새야
+어느 이랑에서나 즐거웁게 솟쳐라.
+
+푸르른 하늘은
+아른아른 높기도 한데`;
+
+const translated = `Spring runs within blood vessels like a stream,
+and on the bank near a stream
+forsythias, azaleas, and yellow cabbage flowers
+
+I, who have endured winter,
+sprout like grass
+
+Joyful robin,
+fly up from any furrow
+
+The blue sky
+glistens high above`;
+
+export default {
+  author: "윤동주 / Yun Dong-Ju",
+  date: "1948",
+  type: "poetry",
+  translations: {
+    original: {
+      language: "ko",
+      title: "봄",
+      content: original,
+    },
+    translated: {
+      language: "en",
+      title: "Spring",
+      content: translated,
+    },
+  },
+} as Work;
